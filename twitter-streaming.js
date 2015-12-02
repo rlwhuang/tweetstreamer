4b30b5ac2dc15508d822d891b49afe4538cbcc37
Richard Lewei Huang <lh1677@nyu.edu>
2015-12-02 02:27:20 -0300

    init commit

---
+++ twitter-streaming.js
@@ -0,0 +1,46 @@
+var app = require('express')();
+var Twitter = require('node-twitter');
+var http = require("http");
+var httpserver = http.createServer(app);
+var io = require('socket.io').listen(httpserver);
+ 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+httpserver.listen(8080, function(){
+  console.log('listening on *:3000');
+});
+ 
+
+
+
+var twitterStreamClient = new Twitter.StreamClient(
+    'YdTcFc9eCTmxaH9sPexhnIEe2',
+    'cslO5Krv82Wmbzq7hkRi5ZjVyVBPaEPtKfxCYEVGp2vtFk7AaR',
+    '1597327297-CkipYyRCVqtOBLvo25AGB691jq6SxKG4QB4RCM9',
+    'pAbl3L7y8N8QGiqeZJHfymhTfD5frANAl9dNQHFmFgYLz'
+);
+
+twitterStreamClient.on('close', function() {
+    console.log('Connection closed.');
+});
+twitterStreamClient.on('end', function() {
+    console.log('End of Line.');
+});
+twitterStreamClient.on('error', function(error) {
+    console.log('Error: ' + (error.code ? error.code + ' ' + error.message : error.message));
+});
+twitterStreamClient.on('tweet', function(tweet) {
+    console.log(tweet.text);
+    io.emit('incoming tweet', tweet.text);
+});
+
+twitterStreamClient.start(['#MacriPresidente', 'scioli', 'macri', 'balotage']);
